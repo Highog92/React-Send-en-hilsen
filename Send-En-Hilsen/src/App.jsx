4e6d6ec2227e6f7ceab61ea7f3387ee3795381ef
Highog92 <Madsserritslev@gmail.com>
2023-08-09 11:22:44 +0200

    Update App.jsx

--- Send-En-Hilsen/src/App.jsx
+++ Send-En-Hilsen/src/App.jsx
@@ -5,11 +5,15 @@ import './App.css'
 import { Hilsen } from "./components/Hilsen";
 
 function App() {
- const hello = "Hej med dig, hvordan går det?"
+  const hello = "Hej med dig, hvordan går det? Dette er en props"
+  const helloWorld = "Hello World"
+  const helloYou = "Hello to you"
 
   return (
     <>
-      <Hilsen hello={hello}/> 
+      <Hilsen hello={hello} />
+      <Hilsen hello={helloWorld} />
+      <Hilsen hello={helloYou} />
     </>
   )
 }
